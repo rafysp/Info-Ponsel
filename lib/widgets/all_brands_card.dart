2adcd7a4cc91a097e5270e956246e44f8b501cd4
rafysp <rafysyah23@gmail.com>
2024-05-14 02:51:55 +0700

    feat: Add AllBrandsCard widget for displaying brand cards in the all brands page

---
+++ lib/widgets/all_brands_card.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:info_ponsel/pages/all_brands_page.dart';
+
+class AllBrandsCard extends StatelessWidget {
+  final String name;
+
+  const AllBrandsCard({Key? key, required this.name}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            brandNameToImagePath(name),
+            height: 150,
+            width: 150,
+            errorBuilder: (context, error, stackTrace) {
+              return Container(
+                child: Icon(
+                  Icons.image_not_supported,
+                  size: 48,
+                ),
+              );
+            },
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            name,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.black87,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
