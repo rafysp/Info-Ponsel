ff605a24842a00f3c62cca21aaa84cb30e4665f4
rafysp <rafysyah23@gmail.com>
2024-05-14 02:41:30 +0700

    refactor: Add PhoneDetailModel class for representing phone detail data

---
+++ lib/model/phone_detail_model.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+class DetailPhone {
+  final Map<String, List<AllSpec>> allSpecs;
+
+  DetailPhone({
+    required this.allSpecs,
+  });
+}
+
+class AllSpec {
+  final String title;
+  final String info;
+
+  AllSpec({
+    required this.title,
+    required this.info,
+  });
+}
+
+class PhoneDetailModel {
+  final String? phoneId;
+  final String? imageUrl;
+  final String? phoneName;
+  final String? description;
+  final String? brandName;
+  final Map<String, List<AllSpec>> allSpecs;
+
+  PhoneDetailModel({
+    this.phoneId,
+    this.imageUrl,
+    this.phoneName,
+    this.description,
+    this.brandName,
+    this.allSpecs = const {},
+  });
+
+  PhoneDetailModel.fromJson(Map<String, dynamic>? json)
+      : phoneId = json?['phoneId'],
+        imageUrl = json?['imageUrl'],
+        phoneName = json?['phoneName'],
+        description = json?['description'],
+        brandName = json?['brandName'],
+        allSpecs = Map<String, List<AllSpec>>.from(json?['allSpecs'] ?? {});
+}
