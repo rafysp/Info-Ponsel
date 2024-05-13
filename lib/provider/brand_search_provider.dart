d093785daf7ca4537c66921ecf1dc053eb2e25d8
rafysp <rafysyah23@gmail.com>
2024-05-14 02:51:22 +0700

    feat: Add BrandSearchProvider for managing brand search functionality

---
+++ lib/provider/brand_search_provider.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:info_ponsel/model/service/all_brands_service.dart';
+
+class BrandSearchProvider extends ChangeNotifier {
+  List<dynamic> allBrands = [];
+  List<dynamic> _filteredBrands = [];
+  bool _isLoading = false;
+
+  List<dynamic> get filteredBrands => _filteredBrands;
+  bool get isLoading => _isLoading;
+
+  set filteredBrands(List<dynamic> value) {
+    _filteredBrands = value;
+    notifyListeners();
+  }
+
+  Future<void> fetchData() async {
+    setLoading(true); // Set loading to true before fetching data
+    try {
+      AllBrandsService brandService = AllBrandsService();
+      List<dynamic> brands = await brandService.fetchBrands();
+      allBrands = brands;
+      filteredBrands = allBrands;
+    } catch (e) {
+      print('Error fetching all brands: $e');
+    } finally {
+      setLoading(false); // Set loading to false after fetching data
+    }
+  }
+
+  void searchBrands(String query) {
+    _filteredBrands = allBrands
+        .where(
+            (brand) => brand.name.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+    // Debugging: Print filtered brands
+    notifyListeners();
+  }
+
+  void setLoading(bool loading) {
+    _isLoading = loading;
+  }
+}
