8b3803f2819054cb7c59acc4b73ed2830871074b
rafysp <rafysyah23@gmail.com>
2024-05-14 02:50:06 +0700

    refactor: Update HomePage to use HomeContent widget for improved code organization

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:info_ponsel/widgets/brand/brands.dart';
-import 'package:info_ponsel/widgets/carousel/carousel_slider.dart';
-import 'package:info_ponsel/widgets/ponsel_terbaru/ponsel_terbaru_card.dart';
-import 'package:info_ponsel/widgets/ponsel_terbaru/ponsel_terbaru_model.dart';
+import 'package:info_ponsel/model/ponsel_terbaru_model.dart';
+import 'package:info_ponsel/model/service/ponsel_terbaru_service.dart';
+import 'package:info_ponsel/pages/home_content.dart';
+import 'package:info_ponsel/pages/recommendation_page.dart';
+import 'package:info_ponsel/pages/selected_brands.dart';
+import 'package:info_ponsel/pages/carousel_slider.dart';
+import 'package:info_ponsel/widgets/ponsel_terbaru_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,122 +16,45 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late Future<List<List<PonselTerbaruModel>>> _phoneModelsFuture;
+  int _selectedIndex = 0;
 
   @override
   void initState() {
     super.initState();
-    _phoneModelsFuture = PonselTerbaruModel.fetchPhoneModelsForAllBrands();
+    _phoneModelsFuture =
+        PonselTerbaruService.fetchPhoneModelsForSelectedBrands();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 10.0),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: Row(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.6,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: TextField(
-                        onChanged: (value) {},
-                        decoration: const InputDecoration(
-                          hintText: 'Cari...',
-                          border: InputBorder.none,
-                          prefixIcon: Icon(Icons.search),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 45.0),
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.2,
-                        height: 50,
-                        margin: const EdgeInsets.only(right: 5.0),
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          shape: BoxShape.circle,
-                        ),
-                        child: const Icon(Icons.favorite_border),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 20.0),
-              HomeCarouselSlider(),
-              SizedBox(height: 20.0),
-              Brands(),
-              const SizedBox(height: 20.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text(
-                      'Ponsel Terbaru',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      'Lihat Semua',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 20.0),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: FutureBuilder<List<List<PonselTerbaruModel>>>(
-                  future: _phoneModelsFuture,
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(child: CircularProgressIndicator());
-                    } else if (snapshot.hasError) {
-                      return Center(child: Text('Error: ${snapshot.error}'));
-                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                      return Center(child: Text('No data available'));
-                    } else {
-                      return Row(
-                        children: snapshot.data!.map((brandPhoneModels) {
-                          if (brandPhoneModels.isNotEmpty) {
-                            return Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 8.0),
-                              child: PonselTerbaruCard(
-                                ponselTerbaruModel: brandPhoneModels.first,
-                              ),
-                            );
-                          } else {
-                            return SizedBox(); // Jika brandPhoneModels kosong, tidak menampilkan apapun
-                          }
-                        }).toList(),
-                      );
-                    }
-                  },
-                ),
-              ),
-            ],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.recommend),
+            label: 'Recommendation',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          HomeContent(phoneModelsFuture: _phoneModelsFuture),
+          RecommendationPage(),
+        ],
       ),
     );
   }
