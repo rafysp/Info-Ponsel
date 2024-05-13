92fc5981122dd0ad9ac465f4e35590a6e65bc27d
rafysp <rafysyah23@gmail.com>
2024-05-14 02:42:09 +0700

    feat: Add PhoneDetailService class for fetching phone detail

---
+++ lib/model/service/phone_detail_service.dart
@@ -0,0 +1,46 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:info_ponsel/model/phone_detail_model.dart';
+
+class PhoneService {
+  static Future<DetailPhone?> fetchPhoneDetail(String? phoneId) async {
+    try {
+      Response response = await Dio().get(
+        'https://mobile-phones2.p.rapidapi.com/phones/$phoneId',
+        options: Options(
+          headers: {
+            'X-RapidAPI-Key':
+                'ba10f8876amsh724918adf1fdaecp147319jsn2cf56f4f34f3',
+            'X-RapidAPI-Host': 'mobile-phones2.p.rapidapi.com',
+          },
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        Map<String, dynamic> responseData = response.data;
+
+        Map<String, List<AllSpec>> allSpecs = {};
+        responseData['all_specs'].forEach((category, specs) {
+          List<AllSpec> specList = [];
+          specs.forEach((spec) {
+            specList.add(AllSpec(
+              title: spec['title'],
+              info: spec['info'],
+            ));
+          });
+          allSpecs[category] = specList;
+        });
+
+        return DetailPhone(
+          allSpecs: allSpecs,
+        );
+      } else {
+        // Tampilkan pesan error jika response tidak berhasil
+        return null;
+      }
+    } catch (e) {
+      // Tangani error saat melakukan request
+      return null;
+    }
+  }
+}
