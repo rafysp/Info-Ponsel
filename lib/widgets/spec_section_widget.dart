beea4b7cdf126ebd6a377f054e04c27e823d3954
rafysp <rafysyah23@gmail.com>
2024-05-14 02:47:30 +0700

    feat: Add SpecSectionWidget class for displaying specifications section

---
+++ lib/widgets/spec_section_widget.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:info_ponsel/model/phone_detail_model.dart';
+
+class SpecSectionWidget extends StatelessWidget {
+  final String title;
+  final List<AllSpec>? specs;
+
+  SpecSectionWidget({
+    required this.title,
+    required this.specs,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.blue,
+            ),
+          ),
+          SizedBox(height: 8),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Column(
+              children: specs?.map((spec) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10.0,
+                    vertical: 4.0, // Kurangi padding vertikal menjadi 4.0
+                  ),
+                  child: ListTile(
+                    title: Text(
+                      spec.title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    subtitle: Text(
+                      spec.info,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ),
+                );
+              }).toList() ??
+                  [],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
