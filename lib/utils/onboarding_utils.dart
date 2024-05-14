0c5406f473598e36bad40f73667909a75566d1e2
rafysp <rafysyah23@gmail.com>
2024-05-14 10:21:23 +0700

    Refactor OnboardingUtils class to remove unused code and add token management

--- lib/utils/onboarding_utils.dart
+++ lib/utils/onboarding_utils.dart
@@ -1,9 +1,10 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
+String token = 'token';
+
 class SharedPref {
   static Future<void> createToken() async {
-    // Buat token baru (misalnya, menggunakan UUID)
-    String token = 'generated_token_here'; // Ganti dengan pembangkitan token yang sesuai
+    
 
     // Simpan token di shared preferences
     SharedPreferences prefs = await SharedPreferences.getInstance();
