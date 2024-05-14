a37b10476a6f228d00fe6472ec9ddf3d11c9628e
rafysp <rafysyah23@gmail.com>
2024-05-14 17:18:04 +0700

    Refactor home_page.dart to improve bottom navigation bar rendering

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -4,7 +4,6 @@ import 'package:info_ponsel/model/service/ponsel_terbaru_service.dart';
 import 'package:info_ponsel/screens/home/home_content.dart';
 import 'package:info_ponsel/screens/recommendation/recommendation_page.dart';
 
-
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -32,20 +31,44 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.recommend),
-            label: 'Recommendation',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
-        onTap: _onItemTapped,
+      bottomNavigationBar: FutureBuilder<List<List<PonselTerbaruModel>>>(
+        future: _phoneModelsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return BottomAppBar(
+              child: Container(),
+            );
+          } else if (snapshot.hasError) {
+            return BottomAppBar(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            final bool homeContentLoading = _selectedIndex == 0 &&
+                snapshot.connectionState != ConnectionState.done;
+
+            if (homeContentLoading) {
+              return BottomAppBar(
+                child: Container(),
+              );
+            } else {
+              return BottomNavigationBar(
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.recommend),
+                    label: 'Recommendation',
+                  ),
+                ],
+                currentIndex: _selectedIndex,
+                selectedItemColor: Colors.blue,
+                onTap: _onItemTapped,
+              );
+            }
+          }
+        },
       ),
       body: IndexedStack(
         index: _selectedIndex,
