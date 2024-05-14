c9699c54f33889d20061cc724415696bb241a73d
rafysp <rafysyah23@gmail.com>
2024-05-14 10:21:09 +0700

    feat: Refactor OnboardingWidgets class to OnboardingWidget and update its implementation

--- lib/widgets/onboarding_widgets.dart
+++ lib/widgets/onboarding_widgets.dart
@@ -1,43 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:info_ponsel/model/onboarding_page_model.dart';
 
-class OnboardingWidgets {
-  static Widget widgetImage(String image) {
-    return Expanded(
-      flex: 3,
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Image.asset(
-          image,
-          fit: BoxFit.contain,
-        ),
-      ),
-    );
-  }
+class OnboardingWidget extends StatelessWidget {
+  final OnboardingPageModel page;
 
-  static Widget widgetTitle(String title) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Text(
-        title,
-        style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 24,
-        ),
-      ),
-    );
-  }
+  const OnboardingWidget({
+    required this.page,
+  });
 
-  static Widget widgetDescription(String description) {
-    return Container(
-      constraints: const BoxConstraints(maxWidth: 295),
-      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 9.0),
-      child: Text(
-        description,
-        textAlign: TextAlign.center,
-        style: const TextStyle(
-          fontSize: 16,
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          flex: 3,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Image.asset(
+              page.image,
+              fit: BoxFit.contain,
+            ),
+          ),
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            page.title,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+            ),
+          ),
+        ),
+        Container(
+          constraints: const BoxConstraints(maxWidth: 300),
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 9.0),
+          child: Text(
+            page.description,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
